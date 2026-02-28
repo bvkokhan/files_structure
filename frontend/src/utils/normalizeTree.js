f996bdfe689555250a31d07963d4fd27618839a6
Bogdan Kokhan <b.kokhan@digiscorp.com>
2026-02-28 21:47:21 +0200

    split normalizeTree/buildNode and simplify TreeNode rendering

--- frontend/src/utils/normalizeTree.js
+++ frontend/src/utils/normalizeTree.js
@@ -1,37 +1,60 @@
-function buildNode(name, node, parentPath = '') {
-  if (!node || typeof node !== 'object') {
-    return null;
+const NODE_TYPE_FILE = 'file';
+const NODE_TYPE_FOLDER = 'folder';
+
+function isObject(value) {
+  return Boolean(value) && typeof value === 'object';
+}
+
+function createNodeId(name, parentPath) {
+  return parentPath ? `${parentPath}/${name}` : name;
+}
+
+function createFileNode(name, id) {
+  return {
+    id,
+    name,
+    type: NODE_TYPE_FILE,
+  };
+}
+
+function buildChildren(childrenMap, parentId) {
+  return Object.entries(childrenMap || {})
+    .map(([childName, childNode]) => buildNode(childName, childNode, parentId))
+    .filter(Boolean);
+}
+
+function createFolderNode(name, node, id) {
+  return {
+    id,
+    name,
+    type: NODE_TYPE_FOLDER,
+    children: buildChildren(node.children, id),
+  };
+}
+
+function buildNodeByType(name, node, id) {
+  if (node.type === NODE_TYPE_FILE) {
+    return createFileNode(name, id);
   }
 
-  const id = parentPath ? `${parentPath}/${name}` : name;
-
-  if (node.type === 'file') {
-    return {
-      id,
-      name,
-      type: 'file',
-    };
-  }
-
-  if (node.type === 'folder') {
-    const entries = Object.entries(node.children || {});
-    const children = entries
-      .map(([childName, childNode]) => buildNode(childName, childNode, id))
-      .filter(Boolean);
-
-    return {
-      id,
-      name,
-      type: 'folder',
-      children,
-    };
+  if (node.type === NODE_TYPE_FOLDER) {
+    return createFolderNode(name, node, id);
   }
 
   return null;
 }
 
+function buildNode(name, node, parentPath = '') {
+  if (!isObject(node)) {
+    return null;
+  }
+
+  const id = createNodeId(name, parentPath);
+  return buildNodeByType(name, node, id);
+}
+
 export function normalizeTree(rawTree) {
-  if (!rawTree || typeof rawTree !== 'object') {
+  if (!isObject(rawTree)) {
     return [];
   }
 
